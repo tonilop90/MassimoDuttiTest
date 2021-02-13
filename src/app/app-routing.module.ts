d0e2f3e74a5f8c183bf3ff67c54a2dbeca9adc30
Toni <tonilop90@gmail.com>
2021-02-13 21:35:48 +0100

    Añadir guard a la ruta 'principal' y configurarlo como provider en app.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+// Services 
+import { AuthGuardService } from './guards/authGuard';
 // Components
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
@@ -7,7 +9,7 @@ import { RegisterComponent } from './components/auth/register/register.component
 const routes: Routes = [
   { path: '', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
-  { path: 'principal', loadChildren: () => import(`./components/principal/principal.module`).then(m => m.PrincipalModule) }
+  { path: 'principal', canActivate:[AuthGuardService] ,loadChildren: () => import(`./components/principal/principal.module`).then(m => m.PrincipalModule) }
   // { path: 'ships', loadChildren: () => import(`./components/ships/ships.module`).then(m => m.ShipsModule) }
 ];
 
