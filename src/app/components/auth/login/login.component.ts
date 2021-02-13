df122e448223e2fe2cb5947b02b319ed2cb4a963
Toni <tonilop90@gmail.com>
2021-02-13 21:18:43 +0100

    Recogida de datos del formulario y llamada al servicio para la comprobación de la autenticación

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { IUser } from 'src/app/interfaces/user';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 // JSON
 import usersList from 'src/assets/json/users.json';
@@ -18,10 +19,12 @@ export class LoginComponent implements OnInit {
   users: Array<IUser> = usersList;
   unregistered: boolean = false;
   invalid: boolean = false;
+  authenticationError: boolean = false;
 
   constructor(
     private fb: FormBuilder,
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) { }
 
   ngOnInit(): void {
@@ -34,10 +37,14 @@ export class LoginComponent implements OnInit {
     if (this.loginForm.invalid) { return }
     // TODO : Falta integrar el servicio para autentificar al usuario
     // JSON simulando usuarios
-    var userLogin = this.loginForm.value.username;
-    var filterJson = this.users.filter(function (user) { return user.first_name === userLogin  });
-    if (filterJson.length > 0) {
+    const userLogin = this.loginForm.value.username;
+    const passwordLogin = this.loginForm.value.password;
+    const filterJson = this.users.filter(function (user) { return user.username === userLogin  });
+    const isCorrectLogin  = this.authService.login(filterJson, passwordLogin, userLogin);
+    if (filterJson.length > 0 && isCorrectLogin) {
       this.router.navigate(['/principal/ships'])
+    } else if(!isCorrectLogin){
+      this.authenticationError = true;
     } else {
       this.unregistered = true;
     }
