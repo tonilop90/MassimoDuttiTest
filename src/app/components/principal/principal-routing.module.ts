e54ff27d19e14140962dd8d6a536a2ccfc1b6f52
Toni <tonilop90@gmail.com>
2021-02-13 16:42:19 +0100

    Fixear imports de los componentes page-one y page-dos en modulo principal

--- src/app/components/principal/principal-routing.module.ts
+++ src/app/components/principal/principal-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ShipsComponent } from '../ships/ships.component';
-import { PageOneComponent } from '../page-one/page-one.component';
-import { PageTwoComponent } from '../page-two/page-two.component';
+import { PageOneComponent } from '../pages/page-one/page-one.component';
+import { PageTwoComponent } from '../pages/page-two/page-two.component';
 import { PrincipalComponent } from './principal.component';
 
 const routes: Routes = [
--- src/app/components/principal/principal.module.ts
+++ src/app/components/principal/principal.module.ts
@@ -5,8 +5,8 @@ import { PrincipalComponentsRoutingModule } from './principal-routing.module';
 import { NgxPaginationModule } from 'ngx-pagination';
 // Components
 import { ShipsComponent } from '../ships/ships.component';
-import { PageOneComponent } from '../page-one/page-one.component';
-import { PageTwoComponent } from '../page-two/page-two.component';
+import { PageOneComponent } from '../pages/page-one/page-one.component';
+import { PageTwoComponent } from '../pages/page-two/page-two.component';
 import { ShipsDetailsComponent } from '../ships/ships-details/ships-details.component';
 
 @NgModule({
